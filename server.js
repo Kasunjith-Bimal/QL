e35de980826a8ce46632855f640070b30b5cf8fa
Kasunjith-Bimal <kasunysoft@gmail.com>
2018-09-14 17:12:50 +0530

    change Server.js second time

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/dist'));
-app.listen(process.env.PORT || 8080);
+app.use(express.static(__dirname + '/dist/qLearn'));
+
 
 app.get('/*',function(req,res){
     res.sendFile(path.join(__dirname + '/dist/qLearn/index.html'));
 })
 
-console.log('Console Lisening !');
+app.listen(process.env.PORT || 8080);
