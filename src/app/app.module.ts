4ffb227efb65504efa4aa3213229aecd60eb27e7
Kasunjith-Bimal <kasunysoft@gmail.com>
2018-09-14 12:37:35 +0530

    add naviagtion and configure route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,36 @@ import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import {MatButtonModule} from '@angular/material';
+import { NavigationComponent } from './navigation/navigation.component';
+import { HomeComponent } from './home/home.component';
+import { ExamComponent } from './exam/exam.component';
+import { AdsComponent } from './ads/ads.component';
+import { TutorialComponent } from './tutorial/tutorial.component';
+import { NewsComponent } from './news/news.component';
+import { PublicationComponent } from './publication/publication.component';
+import { BookComponent } from './book/book.component';
+import { AnswerComponent } from './answer/answer.component';
+import {MatInputModule} from '@angular/material/input';
+import { routing } from './app.route';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    HomeComponent,
+    ExamComponent,
+    AdsComponent,
+    TutorialComponent,
+    NewsComponent,
+    PublicationComponent,
+    BookComponent,
+    AnswerComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatInputModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
