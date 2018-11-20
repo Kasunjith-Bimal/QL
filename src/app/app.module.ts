d8d354124e2fe863a056d23ec190dbb489fd6abe
Kasunjith-Bimal <kasunysoft@gmail.com>
2018-11-20 14:14:29 +0530

    basic Ui Strcture added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { BookComponent } from './book/book.component';
 import { AnswerComponent } from './answer/answer.component';
 import {MatInputModule} from '@angular/material/input';
 import { routing } from './app.route';
+import {MatListModule} from '@angular/material/list';
 @NgModule({
   declarations: [
     AppComponent,
@@ -32,6 +33,7 @@ import { routing } from './app.route';
     BrowserAnimationsModule,
     MatButtonModule,
     MatInputModule,
+    MatListModule,
     routing
   ],
   providers: [],
